fb781f42e9a42b254d6571edc7f9b29f6f38f062
Maik Larooij <mklarooij@gmail.com>
2024-01-10 16:50:18 +0100

    Add database Docker

---
+++ database/schema.sql
@@ -0,0 +1,19 @@
+-- initialize the database
+CREATE database IF NOT EXISTS chat;
+
+use chat;
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255),
+    content VARCHAR(255),
+    likes INTEGER,
+    ys INTEGER,
+    created_at timestamp default current_timestamp
+);
+
+-- seed the database
+INSERT INTO messages (username, content, likes, ys)
+VALUES
+    ('admin', 'Welcome to the chat!', 0, 0),
+    ('jack', 'just setting up my y', 12, 1)
+;
