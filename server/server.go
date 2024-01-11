50fb253d6dde6798afea2d8c53bcb494cad6c80f
Floris Bos <57983252+BluntKatana@users.noreply.github.com>
2024-01-11 13:23:09 +0100

    handlers

--- server/server.go
+++ server/server.go
@@ -10,8 +10,155 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-func main() {
+func getAllMessages(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
 
+	messages, err := db.GetAllMessages(database)
+	if err != nil {
+		return c.JSON(http.StatusOK, db.Messages{})
+	}
+	return c.JSON(http.StatusOK, messages)
+}
+
+func getMessage(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
+
+	// Convert id param to int
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
+	}
+	// Retrieve message by id
+	message, err := db.GetMessageById(database, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "message not found")
+	}
+	return c.JSON(http.StatusOK, message)
+}
+
+func createMessage(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
+
+	// Bind message data from request body to Message struct
+	var message db.Message
+	if err := c.Bind(&message); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid message data")
+	}
+	// Add message to database
+	message, err = db.AddMessage(database, message)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
+	}
+	// Return success response
+	return c.JSON(http.StatusCreated, &message)
+}
+
+func likeMessage(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
+
+	// Convert id param to int
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
+	}
+	// Retrieve message by id
+	message, err := db.GetMessageById(database, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "message not found")
+	}
+
+	message.Likes = message.Likes + 1
+
+	// Update message in database
+	message, err = db.UpdateMessage(database, message)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
+	}
+	// Return success response
+	return c.JSON(http.StatusCreated, &message)
+}
+
+func unlikeMessage(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
+
+	// Convert id param to int
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
+	}
+	// Retrieve message by id
+	message, err := db.GetMessageById(database, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "message not found")
+	}
+
+	message.Likes = message.Likes - 1
+
+	// Update message in database
+	message, err = db.UpdateMessage(database, message)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
+	}
+	// Return success response
+	return c.JSON(http.StatusCreated, &message)
+}
+
+func addyMessage(c echo.Context) error {
+	// Initialize database connection.
+	database, err := db.InitializeDB()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
+	}
+	defer database.Close()
+
+	// Convert id param to int
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
+	}
+	// Retrieve message by id
+	message, err := db.GetMessageById(database, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "message not found")
+	}
+
+	message.Ys = message.Ys + 1
+
+	// Update message in database
+	message, err = db.UpdateMessage(database, message)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
+	}
+	// Return success response
+	return c.JSON(http.StatusCreated, &message)
+}
+
+func main() {
 	// Echo instance
 	e := echo.New()
 
@@ -25,170 +172,13 @@ func main() {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	// All messages
-	e.GET("/messages", func(c echo.Context) error {
-
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		messages, err := db.GetAllMessages(database)
-		if err != nil {
-			return c.JSON(http.StatusOK, db.Messages{})
-		}
-		return c.JSON(http.StatusOK, messages)
-	})
-
-	// Message by id
-	e.GET("/messages/:id", func(c echo.Context) error {
-
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		// Convert id param to int
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
-		}
-		// Retrieve message by id
-		message, err := db.GetMessageById(database, id)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "message not found")
-		}
-		return c.JSON(http.StatusOK, message)
-	})
-
-	// Add a message
-	e.POST("/messages", func(c echo.Context) error {
-
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		// example body: { "username": "test", "content": "test", "likes": 0, "ys": 0 }
-		// Bind message data from request body to Message struct
-		var message db.Message
-		if err := c.Bind(&message); err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid message data")
-		}
-
-		// Check length requirements
-		if len(message.Content) > 155 {
-			return echo.NewHTTPError(http.StatusBadRequest, "message content too long")
-		}
-
-		// Add message to database
-		message, err = db.AddMessage(database, message)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
-		}
-		// Return success response
-		return c.JSON(http.StatusCreated, &message)
-	})
-
-	// Update a message: add a like
-	e.PUT("/messages/:id/like", func(c echo.Context) error {
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		// Convert id param to int
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
-		}
-		// Retrieve message by id
-		message, err := db.GetMessageById(database, id)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "message not found")
-		}
-
-		message.Likes = message.Likes + 1
-
-		// Update message in database
-		message, err = db.UpdateMessage(database, message)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
-		}
-		// Return success response
-		return c.JSON(http.StatusCreated, &message)
-	})
-
-	// Update a message: remove a like
-	e.PUT("/messages/:id/unlike", func(c echo.Context) error {
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		// Convert id param to int
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
-		}
-		// Retrieve message by id
-		message, err := db.GetMessageById(database, id)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "message not found")
-		}
-
-		message.Likes = message.Likes - 1
-
-		// Update message in database
-		message, err = db.UpdateMessage(database, message)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
-		}
-		// Return success response
-		return c.JSON(http.StatusCreated, &message)
-	})
-
-	// Update a message: add a y
-	e.PUT("/messages/:id/y", func(c echo.Context) error {
-
-		// Initialize database connection.
-		database, err := db.InitializeDB()
-		if err != nil {
-			return echo.NewHTTPError(http.StatusServiceUnavailable, "something went wrong processing your request")
-		}
-		defer database.Close()
-
-		// Convert id param to int
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid message id (must be a number)")
-		}
-		// Retrieve message by id
-		message, err := db.GetMessageById(database, id)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "message not found")
-		}
-
-		message.Ys = message.Ys + 1
-
-		// Update message in database
-		message, err = db.UpdateMessage(database, message)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "could not process request")
-		}
-		// Return success response
-		return c.JSON(http.StatusCreated, &message)
-	})
+	// Routes
+	e.GET("/messages", getAllMessages)
+	e.GET("/messages/:id", getMessage)
+	e.POST("/messages", createMessage)
+	e.PUT("/messages/:id/like", likeMessage)
+	e.PUT("/messages/:id/unlike", unlikeMessage)
+	e.PUT("/messages/:id/y", addyMessage)
 
 	// Initialize router
 	e.Logger.Fatal(e.Start(":1323"))
