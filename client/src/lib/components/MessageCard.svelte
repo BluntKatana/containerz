e46b0636315a050570ed49e97ce42d9ff4f56119
sandeyez <s.a.f.vanwerkhooven@students.uu.nl>
2024-01-11 16:38:21 +0100

    Add better comments to files + implement dialog to change username

--- client/src/lib/components/MessageCard.svelte
+++ client/src/lib/components/MessageCard.svelte
@@ -10,20 +10,27 @@
 
   export let message: Message;
 
+  // Store whether the post is liked or not.
   let liked = false;
-  let yed = false;
+
+  // Initialize the like and y counts based on the message prop.
+  let likeCount = message.likes;
+  let yCount = message.ys;
 
   onMount(() => {
+    // When the component mounts, check if the current user has already liked it.
     liked = get(likedPostsStore).includes(message.id);
   });
 
   function like() {
+    // If the post is liked, unlike it, otherwise like it.
     if (liked) {
       likeCount--;
     } else {
       likeCount++;
     }
 
+    // Update the likedPostsStore value accordingly, depending on whether the post is liked or not.
     likedPostsStore.update((value) => {
       if (liked) {
         return value.filter((id) => id !== message.id);
@@ -32,6 +39,8 @@
       return [...value, message.id];
     });
 
+    // Send a request to the server to like/unlike the post.
+    // Note that we do not await the response, so we can show optimistic UI updates.
     fetch(`${PUBLIC_API_URL}/messages/${message.id}/${liked ? 'unlike' : 'like'}`, {
       method: 'PUT',
     }).then((response) => {
@@ -40,24 +49,24 @@
       }
     });
 
+    // Toggle the liked variable.
     liked = !liked;
   }
 
   function y() {
+    // Up de y count
     yCount++;
 
+    // Send a request to the server to like/unlike the post.
+    // Note that we do not await the response, so we can show optimistic UI updates.
     fetch(`${PUBLIC_API_URL}/messages/${message.id}/y`, {
       method: 'PUT',
     });
-
-    yed = !yed;
   }
-
-  let likeCount = message.likes;
-  let yCount = message.ys;
 </script>
 
 <Card.Root>
+  <!-- Card header containing the user name and post datetime -->
   <div class="flex justify-between flex-col gap-1 sm:flex-row">
     <Card.Title>{message.username}</Card.Title>
     <Card.Description
@@ -70,10 +79,15 @@
       })}</Card.Description
     >
   </div>
+
+  <!-- Message content -->
   <Card.Content>
     {message.content}
   </Card.Content>
+
+  <!-- Footer of the card, containing buttons to (un)like or y the post. -->
   <div class="flex gap-4">
+    <!-- Like/Unlike button, also showing the count -->
     <button on:click={like} class="flex gap-1 items-center">
       {#if liked}
         <HeartFilled />
@@ -83,6 +97,8 @@
 
       {likeCount}
     </button>
+
+    <!-- Y button, also showing the count. -->
     <button class="flex gap-1 items-center" on:click={y}
       ><div>𝕐</div>
       {yCount}</button
--- client/src/lib/components/MessageForm.svelte
+++ client/src/lib/components/MessageForm.svelte
@@ -5,53 +5,82 @@
   import * as Card from '$lib/components/ui/card';
   import { PUBLIC_API_URL } from '$env/static/public';
   import { onMount } from 'svelte';
-  import { Check } from 'radix-icons-svelte';
+  import { Pencil1 } from 'radix-icons-svelte';
   import Skeleton from './ui/skeleton/skeleton.svelte';
   import { messageStore } from '@/stores';
   import type { Message } from '@/types';
   import { toast } from 'svelte-sonner';
   import { Reload } from 'radix-icons-svelte';
+  import * as Dialog from '$lib/components/ui/dialog';
+  import { Label } from './ui/label';
 
+  // Constant IDs to use later for determining what form is being submitted.
+  const TEXT_AREA_ID = 'post_textarea';
+  const USERNAME_ID = 'username_input';
+
+  // Content of the message
   let content = '';
+
   let username = '';
   let showUsernameInput = false;
-
   let loadingUsername = true;
-
   let usernameValue = username;
 
   let buttonLoading = false;
 
   onMount(() => {
+    // Get the user's Y username from the localStorage.
     const localStorageUsername = localStorage.getItem('y_username');
 
-    loadingUsername = false;
-
+    // If the username is not defined yet, we want to show the username input.
     if (localStorageUsername === null) {
       showUsernameInput = true;
-      return;
+    }
+    // If the username is defined, we want to set the username variable to the localStorage value.
+    else {
+      username = localStorageUsername;
+      usernameValue = username;
     }
 
-    username = localStorageUsername;
+    // At this point we are done loading the username.
+    loadingUsername = false;
 
+    // On mount, create an eventListener for the keydown event.
     function handleKeyDown(event: KeyboardEvent) {
-      if (event.key === 'Enter') {
+      // If the user presses enter and there is an active element, we want to prevent the default behaviour
+      // and determine which form needs to be submitted.
+      if (event.key === 'Enter' && document.activeElement) {
         event.preventDefault();
-        post();
+
+        if (document.activeElement.id === TEXT_AREA_ID) {
+          post();
+          return;
+        }
+
+        if (document.activeElement.id === USERNAME_ID) {
+          handleUsernameSave();
+          return;
+        }
       }
     }
 
+    // Add the eventListener to the document.
     document.addEventListener('keydown', handleKeyDown);
 
+    // On unmount, remove the eventListener from the document.
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   });
 
   async function post() {
+    // If there is no content, or when it is too long, or when there is no username, we want to return.
     if (content.length === 0 || content.length > 255 || !username) return;
 
+    // Set the buttonLoading variable to true, so we can show a loading spinner.
     buttonLoading = true;
+
+    // Send a request to the server to post the message.
     await fetch(`${PUBLIC_API_URL}/messages`, {
       method: 'POST',
       headers: {
@@ -62,51 +91,96 @@
         username,
       }),
     }).then(async (response) => {
+      // If the response is not 201, we want to throw an error.
       if (response.status !== 201) {
         throw new Error('Something went wrong');
       }
 
+      // Parse the response as JSON (async operation)
       const newMessage: Message = await response.json();
 
+      // Add the new message to the front of the messageStore.
       messageStore.update((messages) => {
         return [newMessage, ...messages];
       });
     });
 
+    // Reset the buttonLoading and content variables, and show a toast.
+    // To give the user the feeling that something is happening, we wait 250ms before doing this.
     setTimeout(() => {
       buttonLoading = false;
       content = '';
       toast('Message posted!');
-    }, 500);
+    }, 250);
   }
 
   function handleUsernameSave() {
+    // If the username is empty, we want to return.
     if (usernameValue.length === 0) return;
 
+    // Set the username in the localStorage and set the username variable.
     localStorage.setItem('y_username', usernameValue);
 
+    // Set the username variable and hide the username input.
     username = usernameValue;
+
+    // Hide the username input.
     showUsernameInput = false;
   }
 </script>
 
-<Card.Root class="flex flex-col gap-2 justify-end">
-  {#if loadingUsername}
-    <Skeleton class="w-32 h-5" />
-  {:else if showUsernameInput}
-    <div class="flex gap-2">
-      <Input
-        bind:value={usernameValue}
-        placeholder={username || 'What should you be called on Y?'}
-      />
-      <Button size="icon" disabled={!usernameValue} on:click={handleUsernameSave}>
-        <Check />
-      </Button>
+<!-- Dialog for the user to change their username -->
+<Dialog.Root bind:open={showUsernameInput}>
+  <Dialog.Content class="sm:max-w-[425px]">
+    <Dialog.Header>
+      <Dialog.Title>Edit username</Dialog.Title>
+      <Dialog.Description>
+        Make changes to your profile here. Click save when you're done.
+      </Dialog.Description>
+    </Dialog.Header>
+
+    <div class="grid gap-4 py-4">
+      <div class="grid grid-cols-4 items-center gap-4">
+        <Label for={USERNAME_ID} class="text-right">Username</Label>
+        <Input
+          id={USERNAME_ID}
+          bind:value={usernameValue}
+          class="col-span-3"
+          placeholder="elonmusk"
+        />
+      </div>
     </div>
-  {:else}
-    <Card.Title>{username}</Card.Title>
-  {/if}
+    <Dialog.Footer>
+      <Button type="submit" on:click={handleUsernameSave}>Save changes</Button>
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
+
+<!-- Form to post a new message to Y. -->
+<Card.Root class="flex flex-col gap-2 justify-end">
+  <div class="min-h-8 max-h-8 flex items-center justify-between">
+    <!-- If we are busy fetching the username from localstorage, show a skeleton loading. -->
+    {#if loadingUsername}
+      <Skeleton class="w-32 h-[18px]" />
+      <!-- If the username has loaded, render it in a card title + a button to open the change username dialog. -->
+    {:else}
+      <Card.Title>
+        {username}
+      </Card.Title>
+      <Button
+        variant="ghost"
+        size="icon"
+        on:click={() => (showUsernameInput = true)}
+        class="w-8 h-8"
+      >
+        <Pencil1 class="h-4 w-4" />
+      </Button>
+    {/if}
+  </div>
+
+  <!-- Textarea to write your post -->
   <Textarea
+    id={TEXT_AREA_ID}
     bind:value={content}
     placeholder="What's happening?"
     cols={40}
@@ -114,12 +188,15 @@
     class="resize-none border-none focus:border-none"
     maxlength={255}
   />
+
+  <!-- Button to post the message -->
   <Button
     type="submit"
     on:click={post}
     disabled={content.length === 0 || !username || buttonLoading}
     class="max-w-36 w-full ml-auto"
   >
+    <!-- If the button is loading, we show a spinning icon. -->
     {#if buttonLoading}
       <Reload class="mr-2 h-4 w-4 animate-spin" />
     {/if}
